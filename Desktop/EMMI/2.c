628bb4ba93153aef1906160483646f41d8ce4c4d
Admin <admin@Davids-MacBook-Air.local>
2018-02-25 10:27:04 +0100

    test commit

---
+++ Desktop/EMMI/2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int ggt(int a, int b);
+
+
+int main(void) {
+
+ int x = 100;
+ int y = 200;
+ int ret;
+
+ /* calling a function to get max value */
+ ret = ggt(x, y);
+
+ printf( "ggt is : %d\n", ret );
+
+ return 0;
+}
+
+
+int ggt(int a, int b) {
+
+	int result;
+
+	while(m!=n) {
+		if(m>n) {m=m-n;}
+		else {n=n-m;}
+	}
+
+	result=n;
+
+	return results;
+}
