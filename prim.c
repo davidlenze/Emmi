0bb33f37962f8a5a96b9b3071b8deac7b3d897c9
davidlenze <davidlenze@yahoo.de>
2018-02-26 13:03:59 +0100

    with input

--- prim.c
+++ prim.c
@@ -5,12 +5,21 @@ int prime(int a);
 
 
 int main(void) {
+ int cont=1;
 
-   x=17;
+ while(cont==1) {
+   printf("Input an integer to be checked for primality\n" );
+
+   int x;
+   scanf("%u", &x);
+
    int z=prim(x);
 
    if(z==0) {printf("the integer is not a prime\n");}
    else     {printf("the integer is a prime\n");}
+
+   printf("type 1 to continue, any other number to exit\n");
+   scanf("%u",&cont );
 
  }
 
@@ -22,7 +31,7 @@ int prim(int n) {
 
   int a=2;
   int b=1;
-  int s=(n/2);
+  int s=(sqrt(n));
   if(n==1) {b=0;}
   else {
     while(b==1 && a<=s) {
