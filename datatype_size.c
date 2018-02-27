826091a93047c100d49133b078f20e0814dcbeca
davidlenze <davidlenze@yahoo.de>
2018-02-27 09:57:06 +0100

    Create datatype_size.c

---
+++ datatype_size.c
@@ -0,0 +1,8 @@
+int main (void) {
+
+  unsigned int m;
+  printf("sizeof(unsigned long int) :%d\n", sizeof(unsigned short int) );
+  printf("sizeof(unsigned int) :%d\n", sizeof(m) );
+  printf("sizeof(unsigned long int) :%d\n", sizeof(unsigned long int) );
+
+}
